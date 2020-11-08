4aab63a14e342566f85d9ca66742c3532abacf91
Yanqin Shen <yanqin.shen@thoughtworks.com>
2020-11-08 18:14:29 +0800

    [syq] feat: scoped proxy

--- src/main/java/com/thoughtworks/capability/demospringioccontainer/GreetingService.java
+++ src/main/java/com/thoughtworks/capability/demospringioccontainer/GreetingService.java
@@ -1,9 +1,18 @@
 package com.thoughtworks.capability.demospringioccontainer;
 
+import org.springframework.context.annotation.Scope;
+import org.springframework.context.annotation.ScopedProxyMode;
 import org.springframework.stereotype.Service;
 
+import static org.springframework.beans.factory.config.ConfigurableBeanFactory.SCOPE_PROTOTYPE;
+
 @Service
+@Scope(value = SCOPE_PROTOTYPE, proxyMode = ScopedProxyMode.TARGET_CLASS)
 public class GreetingService {
+
+    public GreetingService() {
+        System.out.println("++++++++++++GreetingService is instantiating++++++++++++++++");
+    }
 
     String sayHi() {
         return "hello world";
