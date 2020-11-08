ec12313054f3d484a99ebd27a8d97dd254a82f10
Yanqin Shen <yanqin.shen@thoughtworks.com>
2020-11-08 18:25:53 +0800

    [syq] feat: bean factory

--- src/main/java/com/thoughtworks/capability/demospringioccontainer/GreetingController.java
+++ src/main/java/com/thoughtworks/capability/demospringioccontainer/GreetingController.java
@@ -1,5 +1,6 @@
 package com.thoughtworks.capability.demospringioccontainer;
 
+import org.springframework.beans.factory.BeanFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,15 +8,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class GreetingController {
 
-    private final GreetingService greetingService;
-
     @Autowired
-    public GreetingController(GreetingService greetingService) {
-        this.greetingService = greetingService;
-    }
+    private BeanFactory beanFactory;
 
     @GetMapping("/greet")
     public String greet() {
+        GreetingService greetingService = beanFactory.getBean(GreetingService.class);;
         return greetingService.sayHi();
     }
 
